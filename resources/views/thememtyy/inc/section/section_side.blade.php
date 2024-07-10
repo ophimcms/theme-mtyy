75547fc8db29d3896fefbc2723a24a3924830265
To Nguyen <32400888+binhnguyen1998822@users.noreply.github.com>
2024-07-10 10:32:54 +0700

    add link

--- resources/views/thememtyy/inc/section/section_side.blade.php
+++ resources/views/thememtyy/inc/section/section_side.blade.php
@@ -1,6 +1,14 @@
 <div class="box-width tv4 wow fadeInUp wr-list">
     <div class="title top10">
-        <h4 class="title-h cor4">{{$item['label']}}</h4>
+        <h4 class="title-h cor4">
+            <a target="_self" href="{{$item['link']}}" class="ds-line22 more">
+                <span>{{$item['label']}}</span>
+                <span class="this-get">
+                    <i class="this-hide">Xem thêm</i>
+                    <i class="fa ds-jiantouyou"></i>
+                </span>
+            </a>
+        </h4>
     </div>
     <div class="flex wrap border-box public-r hide-b-12">
         <div class="ds-r-hide list-swiper-b">
